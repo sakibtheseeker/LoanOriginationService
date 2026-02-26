2f996a62b0a5709d4f29cdb4c22ecb5092bb6fef
Sakib <stsakib123@gmail.com>
2026-02-26 15:26:34 +0530

    Added Scorecardcontroller

---
+++ LoanOriginationService.API/Controllers/ScoreCardController.cs
@@ -0,0 +1,33 @@
+﻿using LoanOriginationService.Application.DTO.Customer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanOriginationService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreCardController : ControllerBase
+    {
+        private readonly CustomerClient _client;
+
+        public ScoreCardController(CustomerClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("customer/{id}")]
+        public async Task<IActionResult> GetScoreCardByCustomer(int id)
+        {
+            var result = await _client.GetCustomerDetailsById(id);
+
+            if (result == null)
+                return NotFound(new { message = "Scorecard not found" });
+
+            return Ok(new
+            {
+                success = true,
+                data = result
+            });
+        }
+    }
+    }
